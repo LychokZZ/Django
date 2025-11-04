cb3b5063e6a3c0b14501d78692acafbd1524d075
IVAN LUCHYK <ivanluch13@gmail.com>
2025-11-04 13:02:28 +0200

    lab3

--- app_blog/admin.py
+++ app_blog/admin.py
@@ -1,3 +1,39 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.shortcuts import get_object_or_404
+from .models import Article, ArticleImage, Category
+from .forms import ArticleImageForm
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category', 'slug')
+    prepopulated_fields = {'slug': ('category',)}  # автозаповнення slug з назви категорії
+    fieldsets = (
+        (None, {'fields': ('category', 'slug')}),
+    )
+
+class ArticleImageInline(admin.TabularInline):
+    model = ArticleImage
+    form = ArticleImageForm
+    extra = 0
+    fieldsets = (
+        (None, {'fields': ('title', 'image')}),
+    )
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'slug', 'main_page')
+    inlines = [ArticleImageInline]
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('category',)
+    fieldsets = (
+        (None, {'fields': ('pub_date', 'title', 'description', 'main_page', 'category')}),
+        ('Додатково', {
+            'classes': ('grp-collapse', 'grp-closed'),
+            'fields': ('slug',),
+        }),
+    )
+
+    def delete_file(self, pk, request):
+        obj = get_object_or_404(ArticleImage, pk=pk)
+        return obj.delete()
--- app_blog/views.py
+++ app_blog/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from app_blog.models import Article
 
 class HomePageView(TemplateView):
+    template_name = 'index.html'
+
     def get(self, request, **kwargs):
-        return render(request, 'index.html', context=None)
+        articles = (
+            Article.objects
+            .select_related('category')
+            .prefetch_related('images')
+            .order_by('-pub_date')
+        )
+        return render(request, self.template_name, {'articles': articles})
